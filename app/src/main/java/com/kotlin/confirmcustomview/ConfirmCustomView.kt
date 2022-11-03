22547b7ef4b6035cfad03bb8ea7640be29819dcb
Han-JeongHyeon <fldkej131@gmail.com>
2022-11-03 18:02:14 +0900

    update

--- app/src/main/java/com/kotlin/confirmcustomview/ConfirmCustomView.kt
+++ app/src/main/java/com/kotlin/confirmcustomview/ConfirmCustomView.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Build
 import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
 import android.util.Log
 import android.view.KeyEvent
@@ -12,16 +13,20 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
+import org.w3c.dom.Text
 import java.lang.Exception
+import kotlin.math.log
 
 /*
 * 에딧 배경, 커서 커스텀 가능하게
 * 지우기 한번에
 * 내가 입력한 값 가져오기
+* 글자 사이즈, 에딧 크기 변경
 * */
 
 @RequiresApi(Build.VERSION_CODES.Q)
@@ -42,27 +47,24 @@ class ConfirmCustomView(context: Context, attrs: AttributeSet) : ConstraintLayou
             context,
             typedArray.getResourceId(R.styleable.ConfirmCustomView_Background, R.drawable.edit_default))
 
+        val width = typedArray.getDimension(R.styleable.ConfirmCustomView_Width,0F)
+
+        val height = typedArray.getDimension(R.styleable.ConfirmCustomView_Height,0F)
+
+        val fontSize = typedArray.getDimension(R.styleable.ConfirmCustomView_FontSize,16F)
+
         for (id in 1..6) {
             var editTextId = getIdentifier(id)
 
             editTextId.textCursorDrawable = cursor
             editTextId.background = background
             editTextId.setSelectAllOnFocus(true)
-
-            editTextId.addTextChangedListener { text: Editable? ->
-                if (text?.length == 1) {
-                    number.add(text.toString().toInt())
-                    if (id != 6) {
-                        editTextId = getIdentifier(id + 1)
-                        editTextId.requestFocus()
-                    } else {
-                        clear()
-                        Log.d("TAG", number.toString())
-                    }
-                }
-            }
+            editTextId.width = width.toInt()
+            editTextId.height = height.toInt()
+            editTextId.textSize = fontSize
 
             editTextId.setOnKeyListener { view, i, keyEvent ->
+
                 if (keyEvent.keyCode == KeyEvent.KEYCODE_DEL &&
                     keyEvent.action == MotionEvent.ACTION_DOWN )
                 {
@@ -75,8 +77,27 @@ class ConfirmCustomView(context: Context, attrs: AttributeSet) : ConstraintLayou
                         editTextId.requestFocus()
                     }
                 }
+                else if (keyEvent.action == MotionEvent.ACTION_DOWN) {
+//                    editTextId.setText("1")
+//                    editTextId.addTextChangedListener { text: Editable? ->
+//                        if (text?.length == 1) {
+//                            number.add(text.toString().toInt())
+//
+//                            if (id == 6) {
+//                                clear()
+//                                Log.d("TAG", number.toString())
+//                            }
 
-                false
+                            try {
+                                editTextId = getIdentifier(id + 1)
+                                editTextId.requestFocus()
+                            } catch (e: Exception) { }
+
+//                        }
+//                    }
+                }
+
+                true
             }
 
         }
@@ -91,7 +112,7 @@ class ConfirmCustomView(context: Context, attrs: AttributeSet) : ConstraintLayou
 
     private fun getIdentifier(id: Int): EditText {
         return findViewById(
-            resources.getIdentifier("edit$id","id","com.kotlin.confirmcustomview")
+            resources.getIdentifier("edit$id","id",context.packageName)
         )
     }
 
